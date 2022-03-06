7ff8274b18e77ada62d91f89cb2a1d40302ac2c0
Alejandro1407 <alejandroalejo714@gmail.com>
2022-03-06 16:12:41 -0600

    first commit

---
+++ app/src/main/java/sv/com/edu/udb/dsm/activities/LoginActivity.java
@@ -0,0 +1,26 @@
+package sv.com.edu.udb.dsm.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import sv.com.udb.dsm.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private final EditText txtA;
+
+    private final EditText txtB;
+
+    public LoginActivity() {
+        super();
+        txtA = findViewById(R.id.txtA);
+        txtB = findViewById(R.id.txtB)
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
