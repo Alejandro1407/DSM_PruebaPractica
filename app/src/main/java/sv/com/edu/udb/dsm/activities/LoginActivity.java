8351fb2c7c1df61a94d8265c9ea1e34e08acc3d1
Alejandro1407 <alejandroalejo714@gmail.com>
2022-03-06 21:19:53 -0600

    [DSM] - Ejercicio 2

--- app/src/main/java/sv/com/edu/udb/dsm/activities/LoginActivity.java
+++ app/src/main/java/sv/com/edu/udb/dsm/activities/LoginActivity.java
@@ -2,21 +2,10 @@ package sv.com.edu.udb.dsm.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.EditText;
 
 import sv.com.udb.dsm.R;
 
 public class LoginActivity extends AppCompatActivity {
-
-    private final EditText txtA;
-
-    private final EditText txtB;
-
-    public LoginActivity() {
-        super();
-        txtA = findViewById(R.id.txtA);
-        txtB = findViewById(R.id.txtB)
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
